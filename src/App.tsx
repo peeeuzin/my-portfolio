b0e55b62c0b3bedeec05c19112f6fbc591924227
pedrinfx <canaldopedrinho233@gmail.com>
2022-06-25 18:39:11 -0300

    feat: github icon and refactor on portfolio code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-template-curly-in-string */
 import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 import './App.scss';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import styles from './App.module.scss';
@@ -7,11 +8,33 @@ import theme from './theme/theme';
 import { TerminalComponent } from './components/Terminal';
 
 function App() {
-    const codeString =
-        "import pedrinfx from 'pedrinfx';\n\nconst name = pedrinfx.name;\nconst age = pedrinfx.age;\nconst skills = pedrinfx.skills;\n\nconsole.log(`pedrinfx's name: ${name}`);\nconsole.log(`pedrinfx's age: ${age}`);\nconsole.log(`pedrinfx's skills: ${skills}`);\n\nconsole.log(`${name} is current learning: ${pedrinfx.getCurrentLearning()}`);";
+    const codeString = `
+import pedrinfx from 'pedrinfx';
+
+const { name, age, skills } = pedrinfx;
+
+console.log(\`pedrinfx's name: \${name}\`);
+console.log(\`pedrinfx's age: \${age}\`);
+console.log(\`pedrinfx's skills: \${skills}\`);
+
+console.log(\`\${name} is current learning: \${pedrinfx.getCurrentLearning()}\`);
+`;
+
+    const myGithub = 'https://github.com/pedrinfx';
 
     return (
         <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <a href={myGithub}>
+                    <AiFillGithub
+                        size={40}
+                        style={{
+                            color: '#fff',
+                        }}
+                    />
+                </a>
+            </div>
+
             <div className={styles.center}>
                 <div>
                     <h1
@@ -32,75 +55,77 @@ function App() {
                         ? 👋
                     </h1>
                 </div>
-                <div className={styles.highlighter}>
-                    <div className={styles.header}>
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <div
-                                style={{
-                                    background: '#E96379',
-                                    width: 12,
-                                    height: 12,
-                                    borderRadius: 99,
-                                    marginRight: 5,
-                                }}
-                            />
-                            <div
-                                style={{
-                                    background: '#E7DE79',
-                                    width: 12,
-                                    height: 12,
-                                    borderRadius: 99,
-                                    marginRight: 5,
-                                }}
-                            />
-                            <div
-                                style={{
-                                    background: '#67E480',
-                                    width: 12,
-                                    height: 12,
-                                    borderRadius: 99,
-                                    marginRight: 5,
-                                }}
-                            />
-                        </div>
-
-                        <div
-                            style={{
-                                textAlign: 'center',
-                                width: '85%',
-                                opacity: 0.5,
-                                fontSize: 12,
-                            }}
-                        >
-                            index.js
-                        </div>
-                    </div>
-                    <div className={styles.content}>
-                        <SyntaxHighlighter
-                            language="typescript"
-                            style={theme}
-                            customStyle={{
-                                background: '#000101',
-                            }}
-                        >
-                            {codeString}
-                        </SyntaxHighlighter>
-                    </div>
-                </div>
             </div>
             <div className={styles.content}>
                 <div
                     style={{
                         display: 'flex',
-                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column',
                     }}
                 >
+                    <div className={styles.highlighter}>
+                        <div className={styles.highlighter_header}>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        background: '#E96379',
+                                        width: 12,
+                                        height: 12,
+                                        borderRadius: 99,
+                                        marginRight: 5,
+                                    }}
+                                />
+                                <div
+                                    style={{
+                                        background: '#E7DE79',
+                                        width: 12,
+                                        height: 12,
+                                        borderRadius: 99,
+                                        marginRight: 5,
+                                    }}
+                                />
+                                <div
+                                    style={{
+                                        background: '#67E480',
+                                        width: 12,
+                                        height: 12,
+                                        borderRadius: 99,
+                                        marginRight: 5,
+                                    }}
+                                />
+                            </div>
+
+                            <div
+                                style={{
+                                    textAlign: 'center',
+                                    width: '85%',
+                                    opacity: 0.5,
+                                    fontSize: 12,
+                                }}
+                            >
+                                index.js
+                            </div>
+                        </div>
+                        <div className={styles.content}>
+                            <SyntaxHighlighter
+                                language="typescript"
+                                style={theme}
+                                customStyle={{
+                                    background: '#000101',
+                                }}
+                            >
+                                {codeString}
+                            </SyntaxHighlighter>
+                        </div>
+                    </div>
+
                     <TerminalComponent />
                 </div>
             </div>
